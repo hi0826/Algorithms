c05f3749c22d66003d6920dc957b0687f3b4bd0c
hi0826 <kkkttt555@naver.com>
2020-12-23 18:38:14 +0900

    폴더 정리

---
+++ LeetCode/10.js
@@ -0,0 +1,17 @@
+
+let cur = false;
+let memo = [];
+const isMatch = function(s, p) {
+    initMemo(s.length, p.length);
+    console.log(memo);
+}
+
+const initMemo = function(sLen, pLen) {
+    const property = new Array(pLen).fill(null);
+    for(let i = 0; i < sLen; ++i) {
+        memo.push(property);
+    }
+}
+
+const res = isMatch("abcd", "d*");
+console.log(res);
