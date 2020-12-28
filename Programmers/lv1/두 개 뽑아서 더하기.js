1157a162257dd9624d7b817ae38b8a14200371f0
hi0826 <kkkttt555@naver.com>
2020-12-28 14:49:58 +0900

    Programmers JS lv1 solved

---
+++ Programmers/lv1/두 개 뽑아서 더하기.js
@@ -0,0 +1,10 @@
+const solution = numbers => {
+    const set = new Set();
+    for(let i = 0; i < numbers.length-1; ++i) {
+        for(let j = i+1; j < numbers.length; ++j) {
+            set.add(numbers[i] + numbers[j]);
+        }
+    }
+    const answer = Array.from(set).sort((a, b) => a-b);
+    return answer;
+}
