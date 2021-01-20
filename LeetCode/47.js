1191f8b5ba61d65e60f85fb38b0acbef0df5afc2
hi0826 <kkkttt555@naver.com>
2021-01-20 13:26:23 +0900

    leetcode 47

---
+++ LeetCode/47.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permuteUnique = (nums) => {
+    const answer = backTracking(nums);
+};
+
+const backTracking = (nums) => {
+    
+    const answer = [];
+    const dp = (answer, tempArr, nums) => {
+        if(nums.length === 0) {
+            if( answer.has(tempArr) === false )
+                answer.push(tempArr);
+            return;
+        }
+        for(let i = 0; i < nums.length; ++i) {
+            const t = tempArr.slice();
+            t.push(nums[i]);
+            dp(answer, [...t], nums.filter((v, index) => index !== i));
+        }
+    };
+
+    dp(answer, [], nums);
+    console.log(answer);
+    return answer;
+}
+
+Array.prototype.has = function(arr) {
+    if(this.length === 0) return false;
+    for(let e of this) {
+        let equal = true;
+        for(let i = 0 ; i < e.length; ++i) {
+            if(e[i] !== arr[i]) equal = false;
+            else if(e[i] === arr[i] && equal !== false) equal = true;
+        }
+        if(equal === true) return true;
+    }
+    return false;
+}
+
+permuteUnique([1,1,2]);
