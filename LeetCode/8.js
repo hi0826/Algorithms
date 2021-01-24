d4b059065c61fd6b6b38b83231b827db043aa32f
hi0826 <kkkttt555@naver.com>
2021-01-25 08:46:40 +0900

    leetcode 8

--- LeetCode/8.js
+++ LeetCode/8.js
@@ -3,24 +3,28 @@
  * @return {number}
  */
 const myAtoi = (s) => {
-    if( (s[0] !== "+" && s[0] !== "-" && s[0] !== " ") && isNaN(Number(s[0])) ) return 0; 
     let sign = "";
     let numStr = "";
-    const str = s.trim();
-    for(let i = 0; i < str.length; ++i) {
-        if(isNaN(str[i]) === false) numStr += str[i];
-        else if(isNaN(str[i]) === true) {
-            if(sign !== "") return 0;
-            else if(str[i] === "+") sign = "+";
-            else if(str[i] === "-") sign = "-";
+    for(let c of s) {
+        if(c === " ") {
+            if(numStr !== "") break;
+            if(sign !== "") break;
+            continue;
+        }
+        else if(isNaN(c) === false) numStr += c;
+        else {
+            if(c === "+" || c === "-") {
+                if(sign !== "") break;
+                else if(numStr !== "") break;
+                sign = c;
+            } else if(sign !== "") break;
             else break;
         }
     }
     numStr = sign + numStr;
-    let answer = Number(numStr) || 0;
-    if(answer < Math.pow(-2, 31)) return Math.pow(-2, 31);
-    else if(answer > Math.pow(2, 31)-1) return Math.pow(2, 31)-1;
-    return answer;
+    if(Number(numStr) < Math.pow(-2, 31)) return Math.pow(-2, 31);
+    if(Number(numStr) > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
+    return Number(numStr) || 0;
 };
 
-console.log(myAtoi("+"));
+console.log(myAtoi("  +  413"));
