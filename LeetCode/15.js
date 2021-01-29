ae825ef4cd771b73810afdae8466e2d0ab89fc79
hi0826 <kkkttt555@naver.com>
2021-01-29 21:21:28 +0900

    leetcode 15

---
+++ LeetCode/15.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = (nums) => {
+    const answer = [];
+    if(nums.length <= 1) return answer;
+
+    nums.sort((a, b) => a-b);
+    
+
+};
+
+threeSum([-1,0,1,2,-1,-4]);
