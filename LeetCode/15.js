53815ad6887d5a432df36aecbc8e57ba9de6d3d2
hi0826 <kkkttt555@naver.com>
2021-01-31 15:58:23 +0900

    leetcode 15

--- LeetCode/15.js
+++ LeetCode/15.js
@@ -8,9 +8,34 @@ const threeSum = (nums) => {
 
     nums.sort((a, b) => a-b);
     for(let i = 0 ; i < nums.length; ++i) {
-        
+        const val = -1 * nums[i];
+        let front = 0;
+        let back = nums.length-1;
+        while(front < back) {
+            if(front === i) {
+                front += 1;
+                continue;
+            } else if(back === i) {
+                back -= 1;
+                continue;
+            } if(val === nums[front] + nums[back]) {
+                const arr = [nums[i], nums[front], nums[back]].sort((a, b) => a-b);
+                if(hasArray(answer, arr) === false) answer.push(arr);
+                front += 1;
+                continue;
+            }
+            else if(val < nums[front] + nums[back]) back -=1;
+            else if(val > nums[front] + nums[back]) front += 1;
+        }
     }
-
+    console.log(answer);
 };
 
+const hasArray = (arr1, arr2) => {
+    const strArr2 = JSON.stringify(arr2);
+    for(let arr of arr1) {
+        if(JSON.stringify(arr) === strArr2) return true;
+    }
+    return false;
+}
 threeSum([-1,0,1,2,-1,-4]);
