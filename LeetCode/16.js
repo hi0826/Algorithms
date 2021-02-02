63b1d707705ffe6ceab69082b3fd087da72d273c
hi0826 <kkkttt555@naver.com>
2021-02-02 15:54:27 +0900

    leetcode 16

--- LeetCode/16.js
+++ LeetCode/16.js
@@ -4,5 +4,30 @@
  * @return {number}
  */
 const threeSumClosest = (nums, target) => {
+    nums.sort((a, b) => a-b);
+    let closest = Number.MAX_SAFE_INTEGER;
     
+    for(let i = 0; i < nums.length; ++i) {
+        const val = nums[i];
+        let front = i+1;
+        let back = nums.length-1;
+        while(front < back) {
+            const sum = val + nums[front] + nums[back];
+            console.log(val, nums[front], nums[back]);
+            console.log(sum);
+            if(sum === target) return target;
+            else {
+                const cal = Math.abs(target - sum);
+                if(Math.abs(target - closest) > cal) closest = sum;
+                console.log(cal);
+                console.log(closest);
+                if(target - sum > 0) back -= 1;
+                else if(target - sum <= 0) front += 1;
+            }
+        }
+    }
+    console.log(closest);
+    return closest;
 };
+
+threeSumClosest([0,2,1, -3], 1);
