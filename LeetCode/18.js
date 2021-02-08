73e0ec55cabfaf15627c365a831b2d4999aa6762
hi0826 <kkkttt555@naver.com>
2021-02-08 09:07:14 +0900

    leetcode 18

--- LeetCode/18.js
+++ LeetCode/18.js
@@ -3,8 +3,36 @@
  * @param {number} target
  * @return {number[][]}
  */
-const foursum = (nums, target) => {
+const fourSum = (nums, target) => {
+    const answer = [];
+    const answerSet = new Set();
     
+    nums.sort((a, b) => a - b);
+
+    for(let i = 0 ; i < nums.length; ++i) {
+        for(let j = i + 1 ; j < nums.length; ++j) {
+            let f = j+1;
+            let b = nums.length-1;
+            while(f < b) {
+                const sum = nums[i] + nums[j] + nums[f] + nums[b];
+                const sumArr = [nums[i], nums[j], nums[f], nums[b]];
+                if(sum === target) {
+                    if(!answerSet.has(JSON.stringify(sumArr))) {
+                        answerSet.add(JSON.stringify(sumArr));
+                        answer.push(sumArr);
+                        f += 1;
+                    } else {
+                        f += 1;
+                    }
+                } 
+                else if(sum < target) f += 1;
+                else if(sum > target) b -= 1;
+            }
+        }
+    }
+    console.log(answer);
+    return answer;
 }
 
-foursum([1,0,-1,0,-2,2], 0);
+
+foursum([], 0);
