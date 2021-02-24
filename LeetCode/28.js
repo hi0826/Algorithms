9d720c5d5636203359e20435b7dec9c5d28b4bfa
hi0826 <kkkttt555@naver.com>
2021-02-24 14:15:57 +0900

    leetcode 28

---
+++ LeetCode/28.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = (haystack, needle) => {
+    let answer = -1;
+    answer = haystack.indexOf(needle);
+    return answer;
+};
