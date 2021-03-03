31b24617656091cb60c7a48f04536f0a6232768e
hi0826 <kkkttt555@naver.com>
2021-03-03 14:05:02 +0900

    leetcode 30

--- LeetCode/30.js
+++ LeetCode/30.js
@@ -4,25 +4,30 @@
  * @return {number[]}
  */
 const findSubstring = (s, words) => {
-    const answer = new Set();
-    const length = words[0].length;
-    const strArr = [];
-    const makeStr = (s, words) => {
-        if(words.length === 0) {
-            strArr.push(s);
-            return ;
-        }
-        for(let i = 0 ; i < words.length; ++i) {
-            makeStr(s + words[i], words.filter((v, idx) => idx !== i));
-        }
+  const answer = new Set()
+  const length = words[0].length
+  const strArr = []
+  const makeStr = (s, words) => {
+    if (words.length === 0) {
+      strArr.push(s)
+      return
     }
-    makeStr("", words);
-    console.log(strArr);
-    for(let str of strArr) {
-        for(let i = 0 ; i < s.length; i += length) {
-            const n = s.indexOf(str);
-            if(n !== -1) answer.add(n);
-        }
+    for (let i = 0; i < words.length; ++i) {
+      makeStr(
+        s + words[i],
+        words.filter((v, idx) => idx !== i)
+      )
     }
-    return new Array
-};
+  }
+  makeStr('', words)
+  console.log(strArr)
+  for (let str of strArr) {
+    for (let i = 0; i < s.length; i += length) {
+      const n = s.indexOf(str)
+      if (n !== -1) answer.add(n)
+    }
+  }
+  return new Array()
+}
+
+findSubstring('', [])
